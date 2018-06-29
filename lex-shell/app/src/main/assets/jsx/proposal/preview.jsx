c0afa05d8f3dc3a90b22a12141d4b9910bebec77
lex1982103 <lerrain@gmail.com>
2018-06-30 05:33:14 +0800

    1

--- lex-shell/app/src/main/assets/jsx/proposal/preview.jsx
+++ lex-shell/app/src/main/assets/jsx/proposal/preview.jsx
@@ -31,106 +31,128 @@ class Main extends React.Component {
             return null
         let plan = this.state.plan
         return (
-            <div className="bg-white">
-                <div className="bg-dark">
-                    <div style={{height:"30px"}}></div>
-                    <div className="bg-white ml-3 mr-3" style={{width:"690px", height:"200px", border:"#aaaaaa solid 1px", borderRadius:"16px"}}>
+            <div>
+                <div>
+                    <div>
+                        <img src="https://lifeins.iyunbao.com/static/iyb/images/bobcardif/iyb10004banner.jpg" style={{width:"750px",height:"360px"}}/>
                     </div>
                     <div className="divx">
-                        <div className="pt-2 pb-3 center tc-white" style={{width:"250px", backgroundImage:"url(../images/arrow.png)", backgroundSize:"32px 16px", backgroundRepeat:"no-repeat", backgroundPosition:"center bottom"}}>
-                            <div className="lh-60 text17">{ plan.insurant.name ? plan.insurant.name : "被保险人" }</div>
-                            <div className="lh-40 text14">{ plan.insurant.gender == "M" ? "男" : "女" } { plan.insurant.age }周岁</div>
+                        <div className="divx pl-1 pr-1 pt-2 pb-2 tc-dark" style={{width:"250px", backgroundImage:"url(../images/arrow.png)", backgroundSize:"32px 16px", backgroundRepeat:"no-repeat", backgroundPosition:"center bottom"}}>
+                            <div>
+                                <img src={plan.insurant.gender == "M"?"../images/male.png":"../images/female.png"} style={{width:"100px",height:"100px"}}/>
+                            </div>
+                            <div className="pt-1 pb-1">
+                                <div className="lh-50 text16">{ plan.insurant.name ? plan.insurant.name : "被保险人" }</div>
+                                <div className="lh-30 text13">{ plan.insurant.gender == "M" ? "男" : "女" } { plan.insurant.age }岁</div>
+                            </div>
                         </div>
                     </div>
                 </div>
-
-                <div className="ml-1 mr-1 pt-1">
-                    { !plan ? null : plan.product.map((w, j) => w.parent != null ? null : 
-                        <div className="bg-white mt-1" style={{border:"#aaaaaa solid 1px", borderRadius:"10px"}}>
-                            { plan.product.map((v, i) =>
-                                v == w ?
-                                    <div className="product product-main text16">
-                                        <div style={{height:"70px", display:"flex"}}>
-                                            <img style={{width:"60px", height:"60px", margin:"10px 10px 0 10px"}} src={plan.icons[v.vendor]}></img>
-                                            <div style={{width:"600px", marginTop:"10px"}}>
-                                                <span className="text20 eclipse">{v.name}</span>
+                <div className="bg-white">
+                    <div className="divx pt-3 pl-2 pr-2 pb-2">
+                        <div className="bg-primary" style={{width:"20px", height:"40px"}}></div>
+                        <div className="ml-1 lh-40">保障计划</div>
+                        { plan ? <div className="ml-auto lh-40">首年保费：<span className="tc-red">{plan.premium}</span>元</div> : null }
+                    </div>
+                    <div className="ml-1 mr-1">
+                        { !plan ? null : plan.product.map((w, j) => w.parent != null ? null :
+                            <div className={"bg-white"+(j==0?"":" mt-2")} style={{border:"#dddddd 1px solid"}}>
+                                { plan.product.map((v, i) =>
+                                    v == w ?
+                                        <div className="product product-main text16">
+                                            <div style={{height:"70px", display:"flex"}}>
+                                                <img style={{width:"60px", height:"60px", margin:"10px 10px 0 10px"}} src={plan.icons[v.vendor]}></img>
+                                                <div style={{width:"600px", marginTop:"10px"}}>
+                                                    <span className="text20 eclipse">{v.name}</span>
+                                                </div>
                                             </div>
+                                            <div style={{height:"60px", display:"flex"}}>
+                                                <div className="left">
+                                                </div>
+                                                <div className="middle eclipse">
+                                                    <span>{v.purchase} / {v.insure} / {v.pay}</span>
+                                                </div>
+                                                <div className="right">
+                                                    <span style={{color:"#000"}}>{v.premium}元</span>
+                                                </div>
+                                            </div>
+                                            <div style={{height:"10px"}}></div>
                                         </div>
-                                        <div style={{height:"60px", display:"flex"}}>
+                                    : v.parent == j ?
+                                        <div className="product product-rider text16 br-tl">
                                             <div className="left">
+                                                <span style={{color:"#0a0"}}>附</span>
                                             </div>
                                             <div className="middle eclipse">
-                                                <span>{v.purchase} / {v.insure} / {v.pay}</span>
+                                                <span style={{color:"#000", marginRight:"10px"}}>{v.abbrName}</span>
+                                                <span style={{color:"#aaa"}}></span>
                                             </div>
                                             <div className="right">
                                                 <span style={{color:"#000"}}>{v.premium}元</span>
                                             </div>
                                         </div>
-                                        <div style={{height:"10px"}}></div>
-                                    </div> 
-                                : v.parent == j ?
-                                    <div className="product product-rider text16 br-tl-gray">
-                                        <div className="left">
-                                            <span style={{color:"#0a0"}}>附</span>
-                                        </div>
-                                        <div className="middle eclipse">
-                                            <span style={{color:"#000", marginRight:"10px"}}>{v.abbrName}</span>
-                                            <span style={{color:"#aaa"}}>{v.purchase} / {v.insure} / {v.pay}</span>
-                                        </div>
-                                        <div className="right">
-                                            <span style={{color:"#000"}}>{v.premium}元</span>
-                                        </div>
-                                    </div>
-                                : null
-                            )}
-                        </div>
-                    )}
-                    { !plan ? null :
-                        <div className="text16 lh-80 h-80 tc-dark pr-2 right">
-                            合计：{plan.premium}元
-                        </div>
-                    }
-                </div>
-
-                <div className="ml-1 mr-1 mt-1 pt-2 pb-2 bg-white" style={{border:"#aaaaaa solid 1px", borderRadius:"10px"}}>
-                    { !this.state.liability ? null : this.state.liability.map((v, i) => !v.detail ? null :
-                        <div key={i}>
-                            <div className="text17 lh-60 tc-gray center pl-3 pr-3 ml-3 mr-3 mt-1 mb-2 bg-primary tc-white" style={{borderRadius:"30px"}}>{v.name}</div>
-                            { v.detail.map((w, j) =>
-                                <div className="div text17 pl-1 pr-1 pb-1" key={j}>
-                                    <div className="lh-60 pl-1 tc-primary">★ {w.name}</div>
-                                    { w.detail.map((x, k) => 
-                                        <div className="lh-40" key={k}>
-                                            <div className="divx pb-1" onClick={this.showLiabDetail.bind(this, i, j, k)}>
-                                                <img style={{width:"40px",height:"40px"}} src={x.show?"../images/arrow-7-up.png":"../images/arrow-7-down.png"}/>
-                                                <div className="pl-1" style={{width:"650px"}}>{x.text}</div>
-                                            </div>
-                                            { x.detail.map((y, l) => !x.show || !x.detail ? null :
-                                                <div className="divx text15 lh-30 tc-dark pb-1" key={l}>
-                                                    <div className="center" style={{width:"40px"}}></div>
-                                                    <div className="divx"><div className="title">{y.productAbbrName}</div> <div>{y.text}</div></div>
-                                                </div>
-                                            )}
-                                        </div>
-                                    )}
-                                </div>
-                            )}
-                        </div>
-                    )}
-                </div>
-
-                <div className="ml-1 mr-1 mt-1 pt-2 pb-2 bg-white" style={{border:"#aaaaaa solid 1px", borderRadius:"10px"}}>
-                    { !this.state.chart ? null : this.state.chart.map((v, i) => !v.content ? null :
-                        <div>
-                            <div className="text17 lh-60 tc-gray center pl-3 pr-3 ml-3 mr-3 mt-1 mb-2 bg-primary tc-white" style={{borderRadius:"30px"}}>{v.productName}</div>
-                            <div className="divx">
-                                <BenefitChart ref={"benefitChart"+i} id={"benefitChart"+i} chart={v} years={[-2,-1,0,1,2]}/> 
+                                    : null
+                                )}
                             </div>
-                        </div>
-                    )}
-                </div>
+                        )}
+                    </div>
 
-                <div className="tc-dark p-2 text16">温馨提示：免除保险公司责任条款、犹豫期、解除合同的手续及风险、费用扣除等内容，请您仔细阅读保险合同。本演示说明仅供参考，具体保险责任、免除保险公司责任的条款等内容以正式保险合同为准。</div>
+                    <div className="divx mt-1 p-2">
+                        <div className="bg-primary" style={{width:"20px", height:"40px"}}></div>
+                        <div className="ml-1 lh-40">保险责任</div>
+                    </div>
+                    <div className="ml-1 mr-1 pt-2 pb-2 bg-white" style={{border:"#dddddd 1px solid"}}>
+                        { !this.state.liability ? null : this.state.liability.map((v, i) => !v.detail ? null :
+                            <div key={i}>
+                                <div className="text17 lh-60 center pl-3 pr-3 ml-3 mr-3 mt-1 mb-2 tc-primary bg-white" style={{border:"#00aff9 1px solid", borderRadius:"30px"}}>{v.name}</div>
+                                { v.detail.map((w, j) =>
+                                    <div className="div text17 pl-1 pr-1 pb-1" key={j}>
+                                        <div className="lh-60 pl-1 tc-primary">★ {w.name}</div>
+                                        { w.detail.map((x, k) =>
+                                            <div className="lh-40" key={k}>
+                                                <div className="divx pb-1" onClick={this.showLiabDetail.bind(this, i, j, k)}>
+                                                    <img style={{width:"40px",height:"40px"}} src={x.show?"../images/arrow-7-up.png":"../images/arrow-7-down.png"}/>
+                                                    <div className="pl-1" style={{width:"650px"}}>{x.text}</div>
+                                                </div>
+                                                { x.detail.map((y, l) => !x.show || !x.detail ? null :
+                                                    <div className="divx text15 lh-30 tc-dark pb-1" key={l}>
+                                                        <div className="center" style={{width:"40px"}}></div>
+                                                        <div className="divx"><div className="title">{y.productAbbrName}</div> <div>{y.text}</div></div>
+                                                    </div>
+                                                )}
+                                            </div>
+                                        )}
+                                    </div>
+                                )}
+                            </div>
+                        )}
+                    </div>
+
+                    <div className="divx mt-1 p-2">
+                        <div className="bg-primary" style={{width:"20px", height:"40px"}}></div>
+                        <div className="ml-1 lh-40">利益图表</div>
+                    </div>
+                    <div className="ml-1 mr-1 pt-2 pb-2 bg-white" style={{border:"#dddddd 1px solid"}}>
+                        { !this.state.chart ? null : this.state.chart.map((v, i) => !v.content ? null :
+                            <div>
+                                <div className="text17 lh-60 center pl-3 pr-3 ml-3 mr-3 mt-1 mb-2 tc-primary bg-white" style={{border:"#00aff9 1px solid", borderRadius:"30px"}}>{v.productName}</div>
+                                <div className="divx">
+                                    <BenefitChart ref={"benefitChart"+i} id={"benefitChart"+i} chart={v} years={[-2,-1,0,1,2]}/>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+
+                    <div className="divx mt-1 p-2">
+                        <div className="bg-primary" style={{width:"20px", height:"40px"}}></div>
+                        <div className="ml-1 lh-40">温馨提示</div>
+                    </div>
+                    <div className="ml-1 mr-1 p-1 bg-white tc-dark text15" style={{border:"#dddddd 1px solid"}}>
+                    　　免除保险公司责任条款、犹豫期、解除合同的手续及风险、费用扣除等内容，请您仔细阅读保险合同。本演示说明仅供参考，具体保险责任、免除保险公司责任的条款等内容以正式保险合同为准。
+                    </div>
+
+                    <div className="h-60"></div>
+                </div>
             </div>
         )
     }

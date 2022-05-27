93ea8060b5f4c4c2faf5a33475317f24c9f556d7
lex1982103 <lerrain@gmail.com>
2022-05-27 15:36:07 +0800

    优化了min max函数，支持日期的比较

--- lex-formula/src/lerrain/tool/script/warlock/function/FunctionMax.java
+++ lex-formula/src/lerrain/tool/script/warlock/function/FunctionMax.java
@@ -4,22 +4,49 @@ import lerrain.tool.formula.Factors;
 import lerrain.tool.formula.Function;
 import lerrain.tool.formula.Value;
 
+import java.util.Date;
+
 public class FunctionMax implements Function
 {
 	public Object run(Object[] v, Factors factors)
 	{
 		if (v.length > 0)
 		{
-			double max = Value.valueOf(v[0]).doubleValue();
-			
-			for (int i = 1; i < v.length; i++)
+			for (int j = 0; j < v.length; ++j)
 			{
-				double d = Value.valueOf(v[i]).doubleValue();
-				if (max < d)
-					max = d;
+				if (v[j] instanceof Date)
+				{
+					Date max = (Date) v[j];
+
+					for (int i = j + 1; i < v.length; i++)
+					{
+						if (v[i] == null)
+							continue;
+
+						Date d = ((Date) v[i]);
+						if (max.before(d))
+							max = d;
+					}
+
+					return max;
+				}
+				else if (v[j] instanceof Number)
+				{
+					Number max = (Number) v[j];
+
+					for (int i = j + 1; i < v.length; i++)
+					{
+						if (v[i] == null)
+							continue;
+
+						Number d = (Number) v[i];
+						if (max.longValue() < d.longValue() || max.doubleValue() < d.doubleValue())
+							max = d;
+					}
+
+					return max;
+				}
 			}
-			
-			return Double.valueOf(max);
 		}
 		
 		throw new RuntimeException("错误的max运算");
--- lex-formula/src/lerrain/tool/script/warlock/function/FunctionMin.java
+++ lex-formula/src/lerrain/tool/script/warlock/function/FunctionMin.java
@@ -4,24 +4,50 @@ import lerrain.tool.formula.Factors;
 import lerrain.tool.formula.Function;
 import lerrain.tool.formula.Value;
 
+import java.util.Date;
+
 public class FunctionMin implements Function
 {
 	public Object run(Object[] v, Factors factors)
 	{
 		if (v.length > 0)
 		{
-			double min = Value.valueOf(v[0]).doubleValue();
-			
-			for (int i = 1; i < v.length; i++)
+			for (int j = 0; j < v.length; ++j)
 			{
-				double d = Value.valueOf(v[i]).doubleValue();
-				if (min > d)
-					min = d;
+				if (v[j] instanceof Date)
+				{
+					Date min = (Date) v[j];
+
+					for (int i = j + 1; i < v.length; i++)
+					{
+						if (v[i] == null)
+							continue;
+
+						Date d = ((Date) v[i]);
+						if (min.after(d))
+							min = d;
+					}
+
+					return min;
+				}
+				else if (v[j] instanceof Number)
+				{
+					Number min = (Number) v[j];
+
+					for (int i = j + 1; i < v.length; i++)
+					{
+						if (v[i] == null)
+							continue;
+
+						Number d = (Number) v[i];
+						if (min.longValue() > d.longValue() || min.doubleValue() > d.doubleValue())
+							min = d;
+					}
+
+					return min;
+				}
 			}
-			
-			return Double.valueOf(min);
 		}
-		
-		throw new RuntimeException("错误的min运算");
-	}
+
+		throw new RuntimeException("错误的min运算");	}
 }

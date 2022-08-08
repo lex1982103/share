479b9360a8bdab990c0d9315a54c92c648fdd63d
lerrain <lerrain@gmail.com>
2022-08-09 03:19:59 +0800

    语法异常加入错误位置

--- lex-formula/src/lerrain/tool/script/warlock/function/OptimizedFunction.java
+++ lex-formula/src/lerrain/tool/script/warlock/function/OptimizedFunction.java
@@ -5,7 +5,7 @@ import lerrain.tool.script.warlock.Code;
 
 /**
  * 有的函数需要根据参数的个数来判断是否可以优化，所以需要使用带有参数代码的是否可优化函数
- * 比如time()不可优化，time(date, 'yyyy-MM-dd')如果date是固定的，就可以优化
+ * 比如time()不可优化，time(datestr)如果datestr是固定的，就可以优化
  */
 public interface OptimizedFunction extends Function
 {
